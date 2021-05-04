c1df6fa40be9a3bf225e2f7c198b2b7540ece4b8
lucca-falbo <luccafalbo@gmail.com>
2021-05-04 15:40:49 -0300

    base project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import { hot } from 'react-hot-loader';
-import './App.css';
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello, World! Vamo turmaaa! </h1>
+        <h1> Hello, World!</h1>
       </div>
     );
   }
